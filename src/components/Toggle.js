fd558ee652f6fb8f886d515281fc89595ffe5ba0
Milton Ngeno <ngenomilton10@gmail.com>
2025-08-06 09:52:02 +0300

    adds events and states

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React,{useState}from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  function handleButton(){
+    setButton(button =>!button)
+  }
+  const[button ,setButton] = useState(true)
+  const color = button ? "red" :'white'
+  return <button  style ={{background:color}}  onClick={handleButton}> {button? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
